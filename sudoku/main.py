482343a9060a821e6fc3078ada8b112b70210d48
La Minh Duc <laminhduc200@gmail.com>
2024-04-16 10:39:57 +0700

    add test heuristic

--- sudoku/main.py
+++ sudoku/main.py
@@ -1,6 +1,9 @@
 import dfs
+from heuristic import Heuristic
+import  time
 
-board = [[0,0,0,8,4,0,6,5,0],
+board = [
+      [0,0,0,8,4,0,6,5,0],
       [0,8,0,0,0,0,0,0,9],
       [0,0,0,0,0,5,2,0,1],
       [0,3,4,0,7,0,5,0,6],
@@ -10,4 +13,21 @@ board = [[0,0,0,8,4,0,6,5,0],
       [6,0,0,0,0,0,0,4,0],
       [0,5,2,0,8,6,0,0,0]]
 
-dfs.solve_dfs(board)
+print("dfs:")
+dfs_board = dfs.solve_dfs(board)
+
+print("heuristic:")
+new_board = Heuristic(board)
+start_time = time.time()
+heuristic_board = new_board.solve()
+end_time = time.time()
+print('\n'.join([str(line) for line in heuristic_board]))
+print("elapsed time:", end_time - start_time)
+
+for i in range(9):
+      for j in range(9):
+            if dfs_board[i][j] != heuristic_board[i][j]:
+                  print(f"Fail at position {i} and {j}")
+                  break
+
+print("Success")
